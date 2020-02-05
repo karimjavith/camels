e7bafe7251b8e7e027ff93740c52cbb4104e28c0
Karim <44655984+karimjavith@users.noreply.github.com>
2020-02-05 16:55:10 +0000

    fix(dynamic.link) - iOS dynamic link setup complete

--- app/_shared/date.ts
+++ app/_shared/date.ts
@@ -1,11 +1,22 @@
 class DateService {
+  addZero(num: Number | String) {
+    let value = num
+    if (num < 10) {
+      value = '0' + num
+    }
+    return value
+  }
   toLocalDateFormat(value: string) {
     const parsedDate = new Date(value)
     return `${parsedDate.getFullYear()}/${parsedDate.getMonth() + 1}/${parsedDate.getDate()}`
   }
   toLocalTimeFormat(value: string) {
     const parsedDate = new Date(`1970-01-01T${value}`)
-    return `${parsedDate.getHours()}:${parsedDate.getMinutes()}`
+    return `${this.addZero(parsedDate.getHours())}:${this.addZero(parsedDate.getMinutes())}`
+  }
+  toLocalTimeFormatFromDateTime(value: string) {
+    const parsedDate = new Date(value)
+    return `${this.addZero(parsedDate.getHours())}:${this.addZero(parsedDate.getMinutes())}`
   }
 }
 
