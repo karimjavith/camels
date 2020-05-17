0c92f1858c5c2a522ce102f285f0b4a8da6e625e
karim-sheikh <karim@swoopfunding.com>
2020-05-17 21:59:40 +0100

    feat(profile) - WIP

---
+++ app/_shared/firebase/profile.ts
@@ -0,0 +1,23 @@
+import { httpGet, httpPatch } from '../http/http'
+import { handleResponse, handleException } from '../http/httpHelper'
+
+const baseUrl = 'https://us-central1-camels-dev.cloudfunctions.net/api/profile'
+const getProfile = async (uid: string) => {
+  try {
+    const response = await httpGet(baseUrl + `/${uid}`)
+    return await handleResponse(response)
+  } catch (e) {
+    return handleException(e, 'fetch failed')
+  }
+}
+const updateProfile = async (uid: string, type: string) => {
+  try {
+    const response = await httpPatch(baseUrl + `/${uid}`, {
+      type,
+    })
+    return await handleResponse(response)
+  } catch (e) {
+    return handleException(e, 'Update failed')
+  }
+}
+export { getProfile, updateProfile }
